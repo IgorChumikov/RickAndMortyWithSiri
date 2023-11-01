27a41c29fd601eae2644a655144c8c27ca6fc162
Igor <chumikov.igor@mail.ru>
2023-11-01 16:06:55 +0700

    Add

---
+++ RickAndMortyWithSiri/RickAndMortyWithSiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWithSiriApp.swift
+//  RickAndMortyWithSiri
+//
+//  Created by Игорь Чумиков on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWithSiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
