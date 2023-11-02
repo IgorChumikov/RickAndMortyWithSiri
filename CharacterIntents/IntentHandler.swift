4ae39430378ec63e720b814860c99d9dcc7972f0
Igor <chumikov.igor@mail.ru>
2023-11-02 18:18:33 +0700

    тут

--- CharacterIntents/IntentHandler.swift
+++ CharacterIntents/IntentHandler.swift
@@ -18,19 +18,34 @@ class CharacterSearchIntentHandler: NSObject, CharacterSearchIntentHandling {
     
     // Метод обработки интента. Здесь вы выполняете поиск персонажа по имени.
     func handle(intent: CharacterSearchIntent, completion: @escaping (CharacterSearchIntentResponse) -> Void) {
-        print(intent.characterName!)
-        
-        if let characterName = intent.characterName {
-            completion(CharacterSearchIntentResponse.success(characters: characterName))
-        } else {
-            completion(CharacterSearchIntentResponse.init(code: .failure, userActivity: nil))
-        }
-    }
+         // Получение всех персонажей
+         let characters = service.getCharacters()
+         
+         if let characterName = intent.characterName {
+             if characters.contains(where: { $0.name.lowercased() == characterName.lowercased() }) {
+                 completion(CharacterSearchIntentResponse.success(characters: characterName))
+             } else {
+                 completion(CharacterSearchIntentResponse.init(code: .failure, userActivity: nil))
+             }
+         } else {
+             completion(CharacterSearchIntentResponse.init(code: .failure, userActivity: nil))
+         }
+     }
     
     // Метод позволяет вашему приложению предоставить дополнительные детали и подтверждение пользователю, чтобы удостовериться, что интент будет выполнен правильно.
     func confirm(intent: CharacterSearchIntent, completion: @escaping (CharacterSearchIntentResponse) -> Void) {
         if let characterName = intent.characterName {
-            let response = CharacterSearchIntentResponse(code: .ready, userActivity: nil)
+            // Вам может потребоваться добавить логику для подтверждения ваших действий, например:
+            
+            // 1. Показать пользователю сообщение с предварительными деталями запроса
+            let confirmationMessage = "Вы уверены, что хотите найти персонажа по имени '\(characterName)'?"
+            
+            // 2. Создать userActivity для интента с дополнительными данными, если необходимо
+            let userActivity = NSUserActivity(activityType: "com.example.CharacterSearch")
+            
+            // 3. Создать CharacterSearchIntentResponse с кодом .ready и сообщением подтверждения
+            let response = CharacterSearchIntentResponse(code: .ready, userActivity: userActivity)
+            
             completion(response)
         } else {
             // Если не удалось получить имя персонажа, можно вернуть неудачный ответ.
@@ -38,12 +53,15 @@ class CharacterSearchIntentHandler: NSObject, CharacterSearchIntentHandling {
         }
     }
 
+
     
     // // Метод разрешения параметра "characterName." Здесь можно добавить логику для проверки и разрешения имени персонажа, если необходимо.
     func resolveCharacterName(for intent: CharacterSearchIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
         if let characterName = intent.characterName {
             // Проверка, существует ли персонаж с заданным именем
-            if true { // isCharacterValid(characterName)
+            let characters = service.getCharacters()
+            
+            if characters.contains(where: { $0.name.lowercased() == characterName.lowercased() }) {
                 // Персонаж существует, разрешение успешно
                 completion(INStringResolutionResult.success(with: characterName))
             } else {
